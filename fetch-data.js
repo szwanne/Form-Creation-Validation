35ce46dcbd278c055363e778b55c38146c3ef07c
szwanne <103660814+szwanne@users.noreply.github.com>
2025-06-14 10:20:23 +0200

    Changed the function from an arrow to declarative function

--- fetch-data.js
+++ fetch-data.js
@@ -1,4 +1,29 @@
-const fetchUserData = async () => {
+// const fetchUserData = async () => {
+//   const apiUrl = "https://jsonplaceholder.typicode.com/users";
+//   const dataContainer = document.getElementById("api-data");
+//   try {
+//     const response = await fetch(apiUrl);
+//     const users = await response.json(); //This will await for json parsing
+
+//     dataContainer.innerHTML = ""; //Clear the loading message
+
+//     const userList = document.createElement("ul");
+
+//     users.forEach((user) => {
+//       const listItem = document.createElement("li");
+//       listItem.textContent = user.username;
+//       userList.appendChild(listItem); //This will append to unordered list
+//     });
+//     dataContainer.appendChild(userList);
+//   } catch (error) {
+//     dataContainer.innerHTML = "Failed to load user data.";
+//     console.error("Error fetching user data: error");
+//   }
+// };
+
+// document.addEventListener("DOMContentLoaded", fetchUserData);
+
+async function fetchUserData() {
   const apiUrl = "https://jsonplaceholder.typicode.com/users";
   const dataContainer = document.getElementById("api-data");
   try {
@@ -19,6 +44,6 @@ const fetchUserData = async () => {
     dataContainer.innerHTML = "Failed to load user data.";
     console.error("Error fetching user data: error");
   }
-};
+}
 
 document.addEventListener("DOMContentLoaded", fetchUserData);
